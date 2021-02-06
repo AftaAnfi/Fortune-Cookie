a1c1e435f04bd23109669a7d96a577ff636c0ae9
AftaAnfi <77285400+AftaAnfi@users.noreply.github.com>
2021-02-06 14:48:45 -0700

    Finished Fortune Cookie Assignment

--- FortuneCookie/FortuneCookie.vb
+++ FortuneCookie/FortuneCookie.vb
@@ -1,28 +1,61 @@
 ﻿Option Explicit On
 Option Strict On
+
+'Aftanom Anfilofieff
+'RCET0265
+'Spring 2021
+'Fortune Cookie
+'https://github.com/AftaAnfi/Fortune-Cookie.git
 Module FortuneCookie
-    ReadOnly wisdoms As String() = New String() {"You will breathe deeply", "Something amazing will occur", "You deserved that cookie", "Do something kind and kind will be done back"}
+    ReadOnly wisdoms As String() = New String() {"You will breathe deeply", "Something amazing will occur", "You deserved that cookie", "Give a smile"}
 
     Dim runProgram As Boolean = True
+    Dim readInputString As String
+    Dim tempRND As Integer
 
     Sub Main()
+
         Randomize(Date.Now.Millisecond)
-        Console.WriteLine("Press enter to recieve a wisdom...")
 
         While runProgram
 
+            Console.Clear()
+            Console.WriteLine("Press enter to recieve a wisdom...or press q to exit")
 
+            'get input and make sure it's not q, but if it is Exit application
+            readInputString = Console.ReadLine()
+            If readInputString = "q" Or readInputString = "Q" Then
+                Console.WriteLine("Have a nice day.")
+                System.Threading.Thread.Sleep(1700)
+                End
 
+            End If
+            Console.Clear()
+            Console.WriteLine()
 
+            'get a random number from the wisdoms array
+            tempRND = CInt((wisdoms.Length - 1) * Rnd())
+
+            'Give some flair with a ---------- above
+            For i = 0 To wisdoms(tempRND).Length + 1
+                Console.Write("-")
+            Next
+            Console.WriteLine()
+
+            'Write fortune
+            Console.WriteLine((wisdoms(tempRND).PadLeft(wisdoms(tempRND).Length + 1)))
+
+            'Give the fortune flair with ---------- below
+            For i = 0 To wisdoms(tempRND).Length + 1
+                Console.Write("-")
+            Next
+            Console.WriteLine()
+            Console.WriteLine()
+
+            Console.WriteLine("Press enter to continue...")
             Console.ReadLine()
 
-            Console.WriteLine(wisdoms(CInt((wisdoms.Length - 1) * Rnd())))
-
-
-
         End While
-
-
 
     End Sub
 
